f7d687745ff08dd6e02d453ed37713717bf8b217
Austin <93135983+austinFlipside@users.noreply.github.com>
2023-02-23 13:20:42 -0500

    fix formats (#18)

--- models/deepnftvalue/gold/deepnftvalue__fact_tokens.sql
+++ models/deepnftvalue/gold/deepnftvalue__fact_tokens.sql
@@ -2,23 +2,26 @@
     materialized = 'view',
     persist_docs ={ "relation": true,
     "columns": true },
-    meta ={ 'database_tags':{ 'table':{ 'PROTOCOL': 'DEEPNFTVALUE' }}}
+    meta ={ 'database_tags':{ 'table':{ 'PROTOCOL': 'DEEPNFTVALUE' }} }
 ) }}
 
 SELECT
     collection_name,
     LOWER(contract_address) AS collection_address,
-    token_id,
-    active_offer,
-    attributes,
+    token_id :: INTEGER AS token_id,
+    active_offer :: OBJECT AS active_offer,
+    attributes :: OBJECT AS attributes,
     image,
-    is_flagged,
-    last_sale,
-    owner,
-    rarity,
-    rarity_rank,
-    top_bid,
-    valuation,
+    CASE
+        WHEN is_flagged :: STRING = 'true' THEN TRUE
+        ELSE FALSE
+    END is_flagged,
+    last_sale :: OBJECT AS last_sale,
+    owner :: OBJECT AS owner,
+    rarity :: FLOAT AS rarity,
+    rarity_rank :: INTEGER AS rarity_rank,
+    top_bid :: OBJECT AS top_bid,
+    valuation :: OBJECT AS valuation,
     _inserted_timestamp AS updated_timestamp
 FROM
     {{ ref('silver__dnv_tokens') }}
