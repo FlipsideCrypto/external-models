d83b857f3c162f419537c8d5ea052a601df05767
drethereum <trevor.wenokur@gmail.com>
2024-05-23 16:44:36 -0600

    format

--- models/defillama/bronze/bronze__defillama_stablecoin_supply.sql
+++ models/defillama/bronze/bronze__defillama_stablecoin_supply.sql
@@ -4,52 +4,63 @@
     full_refresh = false,
     tags = ['defillama']
 ) }}
-WITH stablecoin_base AS (
 
-{% for item in range(2) %}
-(
-SELECT
-    stablecoin_id,
-    stablecoin,
-    symbol,
-    live.udf_api(
-        'GET',concat('https://stablecoins.llama.fi/stablecoin/',stablecoin_id),{},{}
-    ) AS read,
-    SYSDATE() AS _inserted_timestamp,
-FROM (
-    SELECT 
-        stablecoin_id,
-        stablecoin,
-        symbol,
-        row_num
-    FROM {{ ref('bronze__defillama_stablecoins') }}
-    WHERE row_num BETWEEN {{ item * 5 + 1 }} AND {{ (item + 1) * 5 }} and row_num > 6
-    )
-    {% if is_incremental() %}
-    WHERE stablecoin_id NOT IN (
+WITH stablecoin_base AS ({% for item in range(50) %}
+    (
+
     SELECT
-        stablecoin_id
-    FROM (
-        SELECT 
-            DISTINCT stablecoin_id,
-            MAX(timestamp::DATE) AS max_timestamp
-        FROM {{ this }}
-        GROUP BY 1
-        HAVING CURRENT_DATE = max_timestamp
-    ))
-{% endif %}
-){% if not loop.last %}
+        stablecoin_id, 
+        stablecoin, 
+        symbol, 
+        live.udf_api('GET',
+            CONCAT('https://stablecoins.llama.fi/stablecoin/', stablecoin_id),{},{}) AS READ, 
+        SYSDATE() AS _inserted_timestamp,
+    FROM
+        (
+    SELECT
+        stablecoin_id, 
+        stablecoin, 
+        symbol, 
+        row_num
+    FROM
+        {{ ref('bronze__defillama_stablecoins') }}
+    WHERE
+        row_num BETWEEN {{ item * 5 + 1 }}
+        AND {{(item + 1) * 5 }}
+        )
+
+{% if is_incremental() %}
+WHERE
+    stablecoin_id NOT IN (
+SELECT
+    stablecoin_id
+FROM
+    (
+SELECT
+    DISTINCT stablecoin_id, 
+    MAX(TIMESTAMP :: DATE) AS max_timestamp
+FROM
+    {{ this }}
+GROUP BY
+    1
+HAVING
+    CURRENT_DATE = max_timestamp))
+{% endif %}) {% if not loop.last %}
 UNION ALL
 {% endif %}
-{% endfor %}
-)
+{% endfor %})
 SELECT
-    read:data:address::string as address,
-    read:data:symbol::string as symbol,
-    read:data:name::string as name,
-    read:data:id::string as stablecoin_id,
-    value AS value,
-    TO_TIMESTAMP(VALUE:date::INTEGER) AS timestamp,
+    READ :data :address :: STRING AS address,
+    READ :data :symbol :: STRING AS symbol,
+    READ :data :name :: STRING AS NAME,
+    READ :data :id :: STRING AS stablecoin_id,
+    VALUE AS VALUE,
+    TO_TIMESTAMP(
+        VALUE :date :: INTEGER
+    ) AS TIMESTAMP,
     _inserted_timestamp
-FROM stablecoin_base,
-    LATERAL FLATTEN (input=> read:data:tokens) f
+FROM
+    stablecoin_base,
+    LATERAL FLATTEN (
+        input => READ :data :tokens
+    ) f
