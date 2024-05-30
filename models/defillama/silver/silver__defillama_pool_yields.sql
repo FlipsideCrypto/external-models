537d4564fe5bca6e9b5f3cf2d8fb100044a104d6
mattromano <mattromano88@gmail.com>
2024-05-30 10:38:04 -0700

    null variants stuff

--- models/defillama/silver/silver__defillama_pool_yields.sql
+++ models/defillama/silver/silver__defillama_pool_yields.sql
@@ -51,7 +51,8 @@ FINAL AS (
         )
 
 {% if is_incremental() %}
-WHERE _inserted_timestamp::DATE > (
+WHERE
+    _inserted_timestamp :: DATE > (
         SELECT
             MAX(_inserted_timestamp) :: DATE
         FROM
@@ -60,35 +61,41 @@ WHERE _inserted_timestamp::DATE > (
 {% endif %}
 )
 SELECT
-        TIMESTAMP,
-        apy,
-        apy_base,
-        base_7d,
-        apy_base_inception,
-        apy_mean_30d,
-        apy_pct_1d,
-        apy_pct_30d,
-        apy_pct_7d,
-        apy_rewards,
-        chain,
-        COUNT,
-        exposure,
-        il_7d,
-        il_risk,
-        mu,
-        outlier,
-        pool_id,
-        pool_meta,
-        predictions,
-        projects,
-        reward_tokens,
-        sigma,
-        stablecoin,
-        symbol,
-        tvl_usd,
-        underlying_tokens
-        volume_usd_1d,
-        volume_usd_7d,
+    TIMESTAMP,
+    apy,
+    apy_base,
+    base_7d,
+    apy_base_inception,
+    apy_mean_30d,
+    apy_pct_1d,
+    apy_pct_30d,
+    apy_pct_7d,
+    apy_rewards,
+    chain,
+    COUNT,
+    exposure,
+    il_7d,
+    il_risk,
+    mu,
+    outlier,
+    pool_id,
+    pool_meta,
+    predictions,
+    projects,
+    CASE
+        WHEN reward_tokens [0] IS NULL THEN NULL
+        ELSE reward_tokens
+    END AS reward_tokens,
+    sigma,
+    stablecoin,
+    symbol,
+    tvl_usd,
+    CASE
+        WHEN underlying_tokens [0] IS NULL THEN NULL
+        ELSE underlying_tokens
+    END AS underlying_tokens,
+    volume_usd_1d,
+    volume_usd_7d,
     {{ dbt_utils.generate_surrogate_key(
         ['pool_id','chain','timestamp']
     ) }} AS defillama_yield_id,
