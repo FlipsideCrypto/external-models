4d4646c9b9265591441ca1495f89d986e40d9fac
mattromano <mattromano88@gmail.com>
2024-12-03 12:10:49 -0800

    update headers

--- models/defillama/bronze/bronze__defillama_protocol_tvl.sql
+++ models/defillama/bronze/bronze__defillama_protocol_tvl.sql
@@ -12,7 +12,14 @@ WITH api_pull AS (
         PARSE_JSON(
             live.udf_api(
                 'GET',
-                'https://api.llama.fi/lite/protocols2?b=2',{},{}
+                'https://api.llama.fi/lite/protocols2',
+                {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                    'Accept': 'application/json',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    'Referer': 'https://google.com'
+                },
+                {}
             )
         ) :data :protocols AS response,
         SYSDATE() AS _inserted_timestamp
