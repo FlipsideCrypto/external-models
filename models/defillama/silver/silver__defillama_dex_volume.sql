0e4cebde96c835c73589885bdcf756a62299f962
mattromano <mattromano88@gmail.com>
2025-01-13 08:41:26 -0800

    revert to original model

--- models/defillama/silver/silver__defillama_dex_volume.sql
+++ models/defillama/silver/silver__defillama_dex_volume.sql
@@ -1,58 +1,84 @@
 {{ config(
     materialized = 'incremental',
     unique_key = 'id',
+    enabled = false,
     full_refresh = false,
     tags = ['defillama']
 ) }}
 
-WITH api_pull AS (
+WITH all_chains_options_base AS (
 
+SELECT
+    LOWER(VALUE::STRING) AS chain,
+    ROW_NUMBER() OVER (ORDER BY chain) AS row_num, 
+    _inserted_timestamp
+FROM (
     SELECT
-        PARSE_JSON(
-            live.udf_api(
-                'GET',
-                'https://api.llama.fi/overview/dexs?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyVolume',{},{}
-            )
-        ) :data :protocols AS response,
+        live.udf_api(
+            'GET','https://api.llama.fi/overview/options?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=true&dataType=dailyPremiumVolume',{},{}
+            ) AS read,
         SYSDATE() AS _inserted_timestamp
+    ),
+LATERAL FLATTEN (input=> read:data:allChains) 
 ),
-lat_flat AS (
-    SELECT
-        r.value AS VALUE,
-        r.VALUE:displayName::STRING AS protocol,
-        _inserted_timestamp
-    FROM
-        api_pull,
-        LATERAL FLATTEN (
-            input => response
-        ) AS r
-),
-chain_breakdown AS (
-    SELECT
-        k.key AS chain,
-        SYSDATE() :: DATE AS TIMESTAMP,
-        protocol,
-        k.value AS dex_object,
-        v.value :: INTEGER AS daily_volume,
-        _inserted_timestamp
-    FROM
-        lat_flat,
-        LATERAL FLATTEN(
-            input => VALUE :breakdown24h
-        ) k,
-        LATERAL FLATTEN(
-            input => k.value
-        ) v
-)
+
+options_base AS (
+
+{% for item in range(5) %}
+(
 SELECT
     chain,
-    TIMESTAMP,
-    LOWER(protocol) AS protocol,
-    daily_volume,
-    dex_object,
+    live.udf_api(
+        'GET',CONCAT('https://api.llama.fi/overview/options/',chain,'?excludeTotalDataChart=true&excludeTotalDataChartBreakdown=false&dataType=dailyPremiumVolume'),{},{}
+    ) AS read,
+    SYSDATE() AS _inserted_timestamp
+FROM (
+    SELECT 
+        DISTINCT chain, 
+        row_num
+    FROM all_chains_options_base
+    WHERE row_num BETWEEN {{ item * 5 + 1 }} AND {{ (item + 1) * 5 }}
+    )
+{% if is_incremental() %}
+WHERE chain NOT IN (
+    SELECT
+        chain
+    FROM (
+        SELECT 
+            DISTINCT chain,
+            MAX(timestamp::DATE) AS max_timestamp
+        FROM {{ this }}
+        GROUP BY 1
+        HAVING CURRENT_DATE = max_timestamp
+    )
+)
+{% endif %}
+) {% if not loop.last %}
+UNION ALL
+{% endif %}
+{% endfor %}
+),
+
+reads_output AS (
+
+SELECT
+    chain,
+    TO_TIMESTAMP(VALUE[0]::INTEGER) AS timestamp,
+    VALUE[1] AS options_object,
+    _inserted_timestamp
+FROM options_base,
+    LATERAL FLATTEN (input=> read:data:totalDataChartBreakdown)
+)
+
+SELECT
+    chain,
+    timestamp,
+    key::STRING AS protocol, 
+    value::INTEGER AS daily_volume_premium,
+    options_object,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(
         ['chain', 'protocol', 'timestamp']
     ) }} AS id
-FROM
-    chain_breakdown
+FROM reads_output,
+  LATERAL FLATTEN(input => PARSE_JSON(reads_output.options_object))
