0b246dc29d6712263563fe84156dd4b83f88b7a1
gregoriustanleyy <gstanleytejakusuma@gmail.com>
2025-03-26 21:17:02 +0700

    init metrics + realtime model

---
+++ models/artemis/streamline/streamline__artemis_metrics.sql
@@ -0,0 +1,113 @@
+{{ config(
+    materialized = 'view',
+    tags = ['streamline_view']
+) }}
+
+-- Chains: Scroll, Sui, Stacks, Cardano, Celo, Linea, MultiversX, Starknet, Immutable, Celestia, Wormhole, Acala, Astar, zkSync, Gnosis, Metis, Centrifuge, Fantom, Sonic, Moonbeam, Unichain, Algorand, Berachain
+-- Metrics: DAILY_TXNS (tx_count), DAU (active_users), DAU Cumulative for (TAM? - might introduce double counting since unique wallets are counted daily)
+
+WITH chains AS (
+
+    SELECT
+        'scroll' AS artemis_id
+    UNION ALL
+    SELECT
+        'sui'
+    UNION ALL
+    SELECT
+        'stacks'
+    UNION ALL
+    SELECT
+        'cardano'
+    UNION ALL
+    SELECT
+        'celo'
+    UNION ALL
+    SELECT
+        'linea'
+    UNION ALL
+    SELECT
+        'multiversx'
+    UNION ALL
+    SELECT
+        'starknet'
+    UNION ALL
+    SELECT
+        'immutable_x'
+    UNION ALL
+    SELECT
+        'celestia'
+    UNION ALL
+    SELECT
+        'wormhole'
+    UNION ALL
+    SELECT
+        'acala'
+    UNION ALL
+    SELECT
+        'astar'
+    UNION ALL
+    SELECT
+        'zksync'
+    UNION ALL
+    SELECT
+        'gnosis'
+    UNION ALL
+    SELECT
+        'metis'
+    UNION ALL
+    SELECT
+        'centrifuge'
+    UNION ALL
+    SELECT
+        'fantom'
+    UNION ALL
+    SELECT
+        'sonic'
+    UNION ALL
+    SELECT
+        'moonbeam'
+    UNION ALL
+    SELECT
+        'unichain'
+    UNION ALL
+    SELECT
+        'algorand'
+    UNION ALL
+    SELECT
+        'berachain'
+),
+metrics AS (
+    SELECT
+        artemis_id AS blockchain,
+        'tx_count' AS metric,
+        '{Service}/data/' AS url,
+        'daily_txns' AS endpoint,
+        'Daily transaction count' AS description
+    FROM
+        chains
+    UNION ALL
+    SELECT
+        artemis_id AS blockchain,
+        'active_users' AS metric,
+        '{Service}/data/' AS url,
+        'dau' AS endpoint,
+        'Daily active users count' AS description
+    FROM
+        chains
+)
+SELECT
+    date_day,
+    blockchain,
+    metric,
+    endpoint,
+    url,
+    TO_CHAR(date_day, 'YYYY-MM-DD') AS start_date,
+    TO_CHAR(date_day + INTERVAL '1 day', 'YYYY-MM-DD') AS end_date,
+    description
+FROM
+    {{ source(
+        'crosschain_core',
+        'dim_dates'
+    ) }}
+    CROSS JOIN metrics
---
+++ models/artemis/streamline/streamline__artemis_realtime.sql
@@ -0,0 +1,42 @@
+{{ config (
+    materialized = "view",
+    post_hook = fsc_utils.if_data_call_function_v2(
+        func = 'streamline.udf_bulk_rest_api_v2',
+        target = "{{this.schema}}.{{this.identifier}}",
+        params ={ "external_table" :"artemis",
+        "sql_limit" :"100",
+        "producer_batch_size" :"100",
+        "worker_batch_size" :"100",
+        "async_concurrent_requests": "10",
+        "sql_source" :"{{this.identifier}}",
+        "order_by_column": "date_day" }
+    ),
+    tags = ['streamline_realtime']
+) }}
+
+WITH metrics AS (
+
+    SELECT
+        date_day,
+        blockchain,
+        metric,
+        url,
+        endpoint,
+        TO_CHAR(date_day, 'YYYY-MM-DD') AS query_date
+    FROM
+        {{ ref("streamline__artemis_metrics") }}
+)
+SELECT
+    TO_NUMBER(to_char(date_day, 'YYYYMMDD')) AS date_day,
+    blockchain,
+    metric,
+    TO_NUMBER(to_char(SYSDATE() :: DATE, 'YYYYMMDD')) AS partition_key,
+    {{ target.database }}.live.udf_api(
+        'GET',
+        url || endpoint || '?APIKey={Authentication}' || '&artemisIds=' || blockchain || '&startDate=' || query_date || '&endDate=' || query_date,
+        {},     
+        {},
+        'Vault/prod/external/artemis'
+    ) AS request
+FROM
+    metrics
