4bbad59397c24ac8e3cdb559074a34721c085c5b
gregoriustanleyy <gtejakus@purdue.edu>
2025-04-03 23:53:58 +0700

    upd batching logic

--- models/artemis/gold/artemis__active_users.sql
+++ models/artemis/gold/artemis__active_users.sql
@@ -1,61 +1,32 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['date_day', 'blockchain'],
+    unique_key = ['block_date', 'blockchain', 'metric'],
     tags = ['artemis']
 ) }}
 
-WITH source_data AS (
-
-    SELECT
-        date_day :: DATE AS as_of_date,
-        blockchain,
-        DATA,
-        _inserted_timestamp
-    FROM
-
-{% if is_incremental() %}
-{{ ref('bronze__artemis') }}
-{% else %}
-    {{ ref('bronze__artemis_FR') }}
-{% endif %}
-WHERE
-    metric = 'tx_count'
-
-{% if is_incremental() %}
-AND _inserted_timestamp >= (
-    SELECT
-        MAX(_inserted_timestamp)
-    FROM {{ this }}
-)
-{% endif %}
-),
-flattened_data AS (
-    SELECT
-        as_of_date,
-        blockchain,
-        dau.value:date::DATE AS dau_date,
-        dau.value:val::INT AS dau_count,
-        _inserted_timestamp
-    FROM 
-        source_data,
-        LATERAL FLATTEN(INPUT => DATA:data:artemis_ids[blockchain]:dau) AS dau
-    WHERE 
-        dau.value:val IS NOT NULL
-)
 SELECT
     blockchain,
     'active_users' AS metric,
-    'The reported number of active users on the as_of_date' AS description,
-    as_of_date AS block_date,
-    active_users,
+    'The reported number of active users as of the block_date' AS description,
+    metric_date AS block_date,
+    metric_value AS active_users,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(['blockchain', 'metric', 'block_date']) }} AS active_users_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
-FROM
-    flattened_data qualify ROW_NUMBER() over (
-        PARTITION BY tx_count_id
+FROM 
+    {{ ref('silver__artemis') }}
+WHERE
+    metric = 'dau'
+    {% if is_incremental() %}
+    AND _inserted_timestamp >= (
+        SELECT COALESCE(MAX(_inserted_timestamp), '1970-01-01'::TIMESTAMP_NTZ)
+        FROM {{ this }}
+    )
+    {% endif %}
+QUALIFY ROW_NUMBER() OVER (
+        PARTITION BY active_users_id
         ORDER BY
             _inserted_timestamp DESC
     ) = 1
--- models/artemis/gold/artemis__tx_count.sql
+++ models/artemis/gold/artemis__tx_count.sql
@@ -1,60 +1,33 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['date_day', 'blockchain'],
+    unique_key = ['block_date', 'blockchain', 'metric'],
     tags = ['artemis']
 ) }}
 
-WITH source_data AS (
-
-    SELECT
-        date_day :: DATE AS as_of_date,
-        blockchain,
-        DATA,
-        _inserted_timestamp
-    FROM
-
-{% if is_incremental() %}
-{{ ref('bronze__artemis') }}
-{% else %}
-    {{ ref('bronze__artemis_FR') }}
-{% endif %}
-WHERE
-    metric = 'tx_count'
-
-{% if is_incremental() %}
-AND _inserted_timestamp >= (
-    SELECT
-        MAX(_inserted_timestamp)
-    FROM {{ this }}
-)
-{% endif %}
-),
-flattened_data AS (
-    SELECT
-        as_of_date,
-        blockchain,
-        tx.value:date::DATE AS tx_date,
-        tx.value:val::INT AS tx_count,
-        _inserted_timestamp
-    FROM 
-        source_data,
-        LATERAL FLATTEN(INPUT => DATA:data:artemis_ids[blockchain]:daily_txns) AS tx
-    WHERE 
-        tx.value:val IS NOT NULL
-)
 SELECT
     blockchain,
     'tx_count' AS metric,
-    'The reported number of transactions on the as_of_date' AS description,
-    as_of_date AS block_date,
-    tx_count,
+    'The reported number of transactions as of the block_date' AS description,
+    metric_date AS block_date,
+    metric_value AS tx_count,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(['blockchain', 'metric', 'block_date']) }} AS tx_count_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
-FROM
-    flattened_data qualify ROW_NUMBER() over (
+FROM 
+    {{ ref('silver__artemis') }}
+WHERE
+    metric = 'daily_txns'
+
+    {% if is_incremental() %}
+    AND _inserted_timestamp >= (
+        SELECT COALESCE(MAX(_inserted_timestamp), '1970-01-01'::TIMESTAMP_NTZ)
+        FROM {{ this }}
+    )
+    {% endif %}
+
+QUALIFY ROW_NUMBER() OVER (
         PARTITION BY tx_count_id
         ORDER BY
             _inserted_timestamp DESC
