755a296894e08fc2f86feefa4f15e9aeaa983c5d
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2025-04-10 14:37:26 -0400

    rm nulls (#99)

--- models/artemis/gold/artemis__tx_count.sql
+++ models/artemis/gold/artemis__tx_count.sql
@@ -9,26 +9,27 @@ SELECT
     'tx_count' AS metric,
     'The reported number of transactions as of the block_date' AS description,
     metric_date AS block_date,
-    metric_value AS tx_count,
+    metric_value :: INT AS tx_count,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(['blockchain', 'metric', 'block_date']) }} AS tx_count_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
-FROM 
+FROM
     {{ ref('silver__artemis') }}
 WHERE
     metric = 'daily_txns'
-    AND metric_value IS NOT NULL
+    AND NULLIF(TRIM(metric_value), 'null') IS NOT NULL
 
-    {% if is_incremental() %}
-    AND _inserted_timestamp >= (
-        SELECT COALESCE(MAX(_inserted_timestamp), '1970-01-01'::TIMESTAMP_NTZ)
-        FROM {{ this }}
-    )
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        COALESCE(MAX(_inserted_timestamp), '1970-01-01' :: timestamp_ntz)
+    FROM
+        {{ this }})
     {% endif %}
 
-QUALIFY ROW_NUMBER() OVER (
+    qualify ROW_NUMBER() over (
         PARTITION BY tx_count_id
         ORDER BY
             _inserted_timestamp DESC
