94aa9aba9df750b517cda1d34764effe6a7fcb25
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-04-10 14:29:48 -0400

    rm nulls

--- models/artemis/gold/artemis__active_users.sql
+++ models/artemis/gold/artemis__active_users.sql
@@ -9,25 +9,27 @@ SELECT
     'active_users' AS metric,
     'The reported number of active users as of the block_date' AS description,
     metric_date AS block_date,
-    metric_value AS active_users,
+    metric_value :: INT AS active_users,
     _inserted_timestamp,
     {{ dbt_utils.generate_surrogate_key(['blockchain', 'metric', 'block_date']) }} AS active_users_id,
     SYSDATE() AS inserted_timestamp,
     SYSDATE() AS modified_timestamp,
     '{{ invocation_id }}' AS _invocation_id
-FROM 
+FROM
     {{ ref('silver__artemis') }}
 WHERE
     metric = 'dau'
-    AND metric_value IS NOT NULL
-    
-    {% if is_incremental() %}
-    AND _inserted_timestamp >= (
-        SELECT COALESCE(MAX(_inserted_timestamp), '1970-01-01'::TIMESTAMP_NTZ)
-        FROM {{ this }}
-    )
+    AND NULLIF(TRIM(metric_value), 'null') IS NOT NULL
+
+{% if is_incremental() %}
+AND _inserted_timestamp >= (
+    SELECT
+        COALESCE(MAX(_inserted_timestamp), '1970-01-01' :: timestamp_ntz)
+    FROM
+        {{ this }})
     {% endif %}
-QUALIFY ROW_NUMBER() OVER (
+
+    qualify ROW_NUMBER() over (
         PARTITION BY active_users_id
         ORDER BY
             _inserted_timestamp DESC
