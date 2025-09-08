a466d3658919b3d299bb970c06e395ba24b5e2c5
Austin <austin@flipsidecrypto.com>
2025-09-08 14:39:36 -0400

    fix

--- models/defillama/gold/defillama__dim_protocols.sql
+++ models/defillama/gold/defillama__dim_protocols.sql
@@ -1,15 +1,7 @@
 {{ config(
-    materialized = 'view',
-    persist_docs ={ "relation": true,
-    "columns": true },
-    tags = ['defillama'],
-    meta={
-        'database_tags':{
-            'table': {
-                'PROTOCOL': 'DEFILLAMA'
-            }
-        }
-    }
+    materialized = 'incremental',
+    unique_key = 'protocol_id',
+    tags = ['defillama']
 ) }}
 
 SELECT
@@ -23,5 +15,19 @@ SELECT
     chains,
     category,
     num_audits,
-    audit_note
-FROM {{ ref('bronze__defillama_protocols') }}
+    audit_note,
+    tvl,
+    chain_tvls,
+    sysdate() as inserted_timestamp,
+    sysdate() as modified_timestamp,
+    {{ dbt_utils.generate_surrogate_key(
+        ['protocol_id']
+    ) }} AS dim_protocols_id
+FROM {{ ref('silver__defillama_protocols') }}
+WHERE 1=1
+{% if is_incremental() %}
+AND modified_timestamp > (
+    SELECT MAX(modified_timestamp) FROM {{ this }}
+)
+{% endif %}
+QUALIFY ROW_NUMBER() OVER (PARTITION BY dim_protocols_id ORDER BY modified_timestamp DESC) = 1
---
+++ models/defillama/streamline/bronze/bronze__defillama_protocols_FR.sql
@@ -0,0 +1,9 @@
+{{ config (
+    materialized = 'view',
+    tags = ['defillama_streamline']
+) }}
+{{ streamline_external_table_FR_query_v2(
+    model = 'defillama_protocols',
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER)",
+    partition_name = "partition_key"
+) }}
