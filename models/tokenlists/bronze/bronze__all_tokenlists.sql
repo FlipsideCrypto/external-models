68cde7d4259b7772e6c13c7cf57a3074bafc533c
drethereum <trevor.wenokur@gmail.com>
2025-09-22 09:42:05 -0600

    parens

--- models/tokenlists/bronze/bronze__all_tokenlists.sql
+++ models/tokenlists/bronze/bronze__all_tokenlists.sql
@@ -17,8 +17,8 @@ WITH calls AS ({% for item in range(5) %}
         {{ ref('bronze__verified_tokenlist_seed') }}
     WHERE
         is_enabled
-        AND row_num BETWEEN {{ item * 10 + 1 }}
-        AND {{(item + 1) * 10 }}
+        AND (row_num BETWEEN {{ item * 10 + 1 }}
+        AND {{(item + 1) * 10 }})
 
 ) {% if not loop.last %}
 UNION ALL
